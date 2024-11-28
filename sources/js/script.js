75c080cb5e634544f77b510f7b161cc89ff1eee9
Mahjoubech <charkaouielmahjoub50@gmail.com>
2024-11-28 03:34:54 +0100

    the same commit i send

--- sources/js/script.js
+++ sources/js/script.js
@@ -25,5 +25,109 @@ function showpage(hideId,pageId) {
 showpage('add-player','page-add-player');
 
 //
+let player;
+let dataplayers = JSON.parse(localStorage.getItem('succer')) || [];
+fetch("sources/api/players.json")
+.then(res => res.json())
+.then(res => {
+    player = res.players;   
+    localStorage.setItem('succer', JSON.stringify(player));
+    console.log('Données sauvegardées dans localStorage');
+    showplayers();
+})
+let container = document.getElementById('contentPlayers')
+let field = document.querySelector('.container')
+function showplayers(){
+     dataplayers.forEach(data=>{
+        if(data.position === 'GK'){
+            let content = `<div class="positions ${data.position}">
+            <div class="head-card">
+              <div class="position">
+                  <p>${data.rating}</p>
+                  <span>${data.position}</span>
+              </div>
+              <div class="image-player">
+                  <img width="100" src="${data.photo}" alt="">
+              </div>
+          </div>
+          <div class="body-card">
+              <h3>${data.name}</h3>
+              <div class="rate-player">
+                  <div>
+                      <p>PIV</p>
+                      <span>${data.diving}</span>
+                  </div>
+                  <div>
+                      <p>HAN</p>
+                      <span>${data.handling}</span>
+                  </div>
+                  <div>
+                      <p>KIC</p>
+                      <span>${data.kicking}</span>
+                  </div>
+                  <div>
+                      <p>REF</p>
+                      <span>${data.reflexes}</span>
+                  </div>
+                  <div>
+                      <p>SPE</p>
+                      <span>${data.speed}</span>
+                  </div>
+                  <div>
+                      <p>POS</p>
+                      <span>${data.positioning}</span>
+                  </div>
+              </div>
+          </div>
+          </div>`;
+          container.innerHTML += content;  
+        }else{
+            content = `<div class="positions">
+                <div class="head-card">
+                  <div class="position">
+                      <p>${data.rating}</p>
+                      <span>${data.position}</span>
+                  </div>
+                  <div class="image-player">
+                      <img width="100" src="${data.photo}" alt="">
+                  </div>
+              </div>
+              <div class="body-card">
+                  <h3>${data.name}</h3>
+                  <div class="rate-player">
+                      <div>
+                          <p>PAC</p>
+                          <span>${data.pace}</span>
+                      </div>
+                      <div>
+                          <p>SHO</p>
+                          <span>${data.shooting}</span>
+                      </div>
+                      <div>
+                          <p>PAS</p>
+                          <span>${data.passing}</span>
+                      </div>
+                      <div>
+                          <p>DRI</p>
+                          <span>${data.dribbling}</span>
+                      </div>
+                      <div>
+                          <p>DEF</p>
+                          <span>${data.defending}</span>
+                      </div>
+                      <div>
+                          <p>PHY</p>
+                          <span>${data.physical}</span>
+                      </div>
+                  </div>
+              </div>
+              </div>`;
+              container.innerHTML += content;  
+        }
+        
+     })
+}
 
-fetch()
+
+showplayers()
+
