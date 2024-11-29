defe170f4c838b5f90a4e533f70e781781c63878
Mahjoubech <charkaouielmahjoub50@gmail.com>
2024-11-29 01:33:53 +0100

    add function for change player use the position for that

--- sources/js/script.js
+++ sources/js/script.js
@@ -11,10 +11,6 @@ document.getElementById('players').addEventListener('click', () => {
     document.getElementById('add-player').classList.remove('active')
     document.getElementById('formations').classList.remove('active')
     showpage('players','page-players')});
-    document.getElementById('Change').addEventListener('click', () =>{
-        document.getElementById('formations').classList.remove('active')
-        document.getElementById('players').classList.remove('active')
-        showpage('Change','changment')} );
 function showpage(hideId,pageId) {
     document.querySelectorAll('.filtbtn').forEach(button => {
         button.classList.remove('active');
@@ -27,10 +23,61 @@ function showpage(hideId,pageId) {
 }
 
 showpage('add-player','page-add-player');
-
-//
 let player;
 let dataplayers = JSON.parse(localStorage.getItem('succer')) || [];
+////Ajioute players
+let addplayer = document.getElementById('clickadd');
+let nameplayert = document.getElementById('player-name')
+let posplayert = document.getElementById('player-position')
+let photoplayert = document.getElementById('photo-link')
+let ratingplt = document.getElementById('rating')
+let paceplt = document.getElementById('Pace')
+let shootingplt = document.getElementById('Shooting')
+let passingplt = document.getElementById('Passing')
+let dribblingplt = document.getElementById('Dribbling')
+let physiqplt = document.getElementById('Physical')
+
+addplayer.addEventListener('click',()=>{
+    let file = photoplayert.files[0];
+  if (file) {
+      let reader = new FileReader();
+
+      reader.onload = function (event) {
+          let newplayer = {
+            nameplayer: nameplayert.value,
+            posplayer: posplayert.value,
+            ratingpl: ratingplt.value,
+              photoplayer : event.target.result, 
+              pacepl: paceplt.value,
+              shootingpl: shootingplt.value,
+              passingpl: passingplt.value,
+              dribblingpl: dribblingplt.value,
+              physiqpl: physiqplt.value,
+          };
+
+          dataplayers.push(newplayer);
+          localStorage.setItem('succer', JSON.stringify(dataplayers));
+          alert('Player added successfully!');
+          nameplayert.value = "";
+          posplayert.value = "";
+          ratingplt.value = "";
+          photoplayert.value = "";
+          paceplt.value = "";
+          shootingplt.value = "";
+          passingplt.value = "";
+          dribblingplt.value = "";
+          physiqplt.value = "";
+          showplayers()
+      };
+
+      reader.readAsDataURL(file); 
+  } else {
+      alert('please add photo the player');
+  }
+})
+
+
+
 fetch("sources/api/players.json")
 .then(res => res.json())
 .then(res => {
@@ -42,10 +89,35 @@ fetch("sources/api/players.json")
 })
 let container = document.getElementById('contentPlayers')
 let staduim = document.querySelector('.container')
-function showplayers(){
-     dataplayers.forEach(data=>{
+function showplayers() {
+    dataplayers.forEach(data => {
+        // let content = `
+        // <div class="positions" id="${data.name}" onclick='ChangerPlayer(${JSON.stringify(data)})'>
+        //     <div class="head-card">
+        //         <div class="position">
+        //             <p>${data.rating}</p>
+        //             <span>${data.position}</span>
+        //         </div>
+        //         <div class="image-player">
+        //             <img width="100" src="${data.photo}" alt="">
+        //         </div>
+        //     </div>
+        //     <div class="body-card">
+        //         <h3>${data.name}</h3>
+        //         <div class="rate-player">
+        //             <div><p>PAC</p><span>${data.pace || "N/A"}</span></div>
+        //             <div><p>SHO</p><span>${data.shooting || "N/A"}</span></div>
+        //             <div><p>PAS</p><span>${data.passing || "N/A"}</span></div>
+        //             <div><p>DRI</p><span>${data.dribbling || "N/A"}</span></div>
+        //             <div><p>DEF</p><span>${data.defending || "N/A"}</span></div>
+        //             <div><p>PHY</p><span>${data.physical || "N/A"}</span></div>
+        //         </div>
+        //     </div>
+        // </div>`;
+        
+        // container.innerHTML += content;
         if(data.position === 'GK'){
-            let content = `<div class="positions">
+            let content =` <div class="positions" id="${data.name}" onclick='ChangerPlayer(${JSON.stringify(data)})'>
             <div class="head-card">
               <div class="position">
                   <p>${data.rating}</p>
@@ -87,7 +159,7 @@ function showplayers(){
           </div>`;
           container.innerHTML += content;  
         }else{
-            content = `<div class="positions">
+            content =` <div class="positions" id="${data.name}" onclick='ChangerPlayer(${JSON.stringify(data)})'">
                 <div class="head-card">
                   <div class="position">
                       <p>${data.rating}</p>
@@ -128,10 +200,11 @@ function showplayers(){
               </div>
               </div>`;
               container.innerHTML += content;  
-        }
-      
-     })
+ } })
 }
+    
+
+
 
 function selectplayer() {
     let selected = {
@@ -155,6 +228,106 @@ function selectplayer() {
     return selected;
 }
 
+function ChangerPlayer(data) {
+console.log(data.position);
+
+
+    const selectedDiv = document.getElementById(data.position);
+
+    if (selectedDiv) {
+        // Générer le contenu HTML pour le joueur sélectionné
+        if(data.position === 'GK'){
+            let content =` <div class="positions" id="${data.name}" onclick='ChangerPlayer(${JSON.stringify(data)})'>
+            <div class="head-card">
+              <div class="position">
+                  <p>${data.rating}</p>
+                  <span>${data.position}</span>
+              </div>
+              <div class="image-player">
+                  <img width="100" src="${data.photo}" alt="">
+              </div>
+          </div>
+          <div class="body-card">
+              <h3>${data.name}</h3>
+              <div class="rate-player">
+                  <div>
+                      <p>PIV</p>
+                      <span>${data.diving}</span>
+                  </div>
+                  <div>
+                      <p>HAN</p>
+                      <span>${data.handling}</span>
+                  </div>
+                  <div>
+                      <p>KIC</p>
+                      <span>${data.kicking}</span>
+                  </div>
+                  <div>
+                      <p>REF</p>
+                      <span>${data.reflexes}</span>
+                  </div>
+                  <div>
+                      <p>SPE</p>
+                      <span>${data.speed}</span>
+                  </div>
+                  <div>
+                      <p>POS</p>
+                      <span>${data.positioning}</span>
+                  </div>
+              </div>
+          </div>
+          </div>`;
+          selectedDiv.innerHTML = content;
+        }else{
+ content =` <div class="positions" id="${data.name}" onclick='ChangerPlayer(${JSON.stringify(data)})'">
+                <div class="head-card">
+                  <div class="position">
+                      <p>${data.rating}</p>
+                      <span>${data.position}</span>
+                  </div>
+                  <div class="image-player">
+                      <img width="100" src="${data.photo}" alt="">
+                  </div>
+              </div>
+              <div class="body-card">
+                  <h3>${data.name}</h3>
+                  <div class="rate-player">
+                      <div>
+                          <p>PAC</p>
+                          <span>${data.pace}</span>
+                      </div>
+                      <div>
+                          <p>SHO</p>
+                          <span>${data.shooting}</span>
+                      </div>
+                      <div>
+                          <p>PAS</p>
+                          <span>${data.passing}</span>
+                      </div>
+                      <div>
+                          <p>DRI</p>
+                          <span>${data.dribbling}</span>
+                      </div>
+                      <div>
+                          <p>DEF</p>
+                          <span>${data.defending}</span>
+                      </div>
+                      <div>
+                          <p>PHY</p>
+                          <span>${data.physical}</span>
+                      </div>
+                  </div>
+              </div>
+              </div>`;
+        // Remplacer le contenu de la div liée à la position
+        selectedDiv.innerHTML = content;
+        }
+        console.log(`Le joueur ${data.name} a remplacé dans ${data.position}`);
+    } else {
+        console.log("Aucune div trouvée pour cette position.");
+    }
+}
+
 
 console.log(selectplayer());
 function showselected(){
@@ -162,7 +335,7 @@ function showselected(){
     for(let pos in team){
         team[pos].forEach(player=>{
          if(player.position === 'GK'){
-            let content = `<div class="positions ${pos}">
+            let content = `<div class="positions ${pos}" id="${player.position}">
             <div class="head-card">
               <div class="position">
                   <p>${player.rating}</p>
@@ -204,7 +377,7 @@ function showselected(){
           </div>`;
           staduim.innerHTML += content
         }else{
-            content = `<div class="positions ${pos}">
+            content = `<div class="positions ${pos}"  id="${player.position}">
                 <div class="head-card">
                   <div class="position">
                       <p>${player.rating}</p>
@@ -250,6 +423,10 @@ function showselected(){
         
     }
 }
+
+
+
+
 
 
 // Appel des fonctions
